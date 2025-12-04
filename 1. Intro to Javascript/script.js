b72c55989f402cfac205f49f3639ebb1c24a8104
Rajdeep5270 <rajdeepex5270@gmail.com>
2025-12-04 11:07:01 +0530

    Intro to Javascript

---
+++ 1. Intro to Javascript/script.js
@@ -0,0 +1,22 @@
+let name = prompt("Enter your name");
+let age = +prompt("Enter your age");
+let school = prompt("Enter your school name");
+
+console.log(`${name} is ${age} years old & is from ${school}`);
+
+console.log("--------\n");
+console.log("|      |\n");
+console.log("R      |\n");
+console.log("N      |\n");
+console.log("W      |\n");
+console.log("|      |\n");
+console.log("--------\n");
+
+console.log("\n\n");
+
+console.log("*\n");
+console.log("**\n");
+console.log("***\n");
+console.log("**\n");
+console.log("*\n");
+
