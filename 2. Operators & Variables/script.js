2671550af585c167ae5e8d34b0cb86863153ec36
Rajdeep5270 <rajdeepex5270@gmail.com>
2025-12-04 13:43:43 +0530

    Operators & Variables

---
+++ 2. Operators & Variables/script.js
@@ -0,0 +1,16 @@
+document.write("<h1 style='text-align:center;'>Operators & Variables.</h1>");
+
+a = 12;
+b = 6;
+
+console.log(`Addition of ${a} and ${b} is ${a + b}`);
+console.log(`Substraction of ${a} and ${b} is ${a - b}`);
+console.log(`Multiplication of ${a} and ${b} is ${a * b}`);
+console.log(`Division of ${a} and ${b} is ${a / b}`);
+console.log(`Modulo of ${a} and ${b} is ${a % b}\n\n`);
+
+console.log(`${a} + ${b} = ${a + b}`);
+console.log(`${a} - ${b} = ${a - b}`);
+console.log(`${a} * ${b} = ${a * b}`);
+console.log(`${a} / ${b} = ${a / b}`);
+console.log(`${a} % ${b} = ${a % b}`);
