f6bd3aa11ea3aeaaa4925b89fd06d659282b1adc
Rajdeep5270 <rajdeepex5270@gmail.com>
2025-12-15 18:29:20 +0530

    if-else & ladder if-else

---
+++ 5. Control Structure/5-1/script.js
@@ -0,0 +1,20 @@
+// 1.  min max program using if else
+
+// let a = +prompt("Enter any number");
+// let b = +prompt("Enter any number");
+
+// if (a < b)
+//     console.log(`${a} is small.`);
+// else
+//     console.log(`${b} is small.`);
+
+// Q.2 Write a Program to find if a given number is neutral or not using a ladder if else. 
+
+let a = prompt("Enter any number");
+
+if (a > 0)
+    console.log(`${a} is a positive number`);
+else if (a == 0)
+    console.log(`${a} is a neutral number`);
+else
+    console.log(`${a} is a negative number`); 
